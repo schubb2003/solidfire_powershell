e6e2ed21a0f1dc95cf2e5248105ce885dc16fa84
schubb2003 <schubb@gmail.com>
2018-02-28 14:46:46 -0500

    inital commit

---
+++ sf_enable_ldap_config_credential_add.ps1
@@ -0,0 +1,16 @@
+import-module solidfire
+
+$sfCluster = Read-host("Enter a SolidFire cluster to connect to: ")
+$creds = get-credential
+
+$user = $creds.username
+$password = ConvertTo-SecureString $creds.password -AsPlainText -Force
+$cred = New-Object -TypeName system.Management.Automation.PSCredential -ArgumentList $user,$password
+
+Set-SFLdapAuthentication `
+    -AuthType SearchAndBind `
+    -ServerURIs 'ldap://dc1.demo.netapp.com' `
+    -UserSearchBaseDN "DC=demo,DC=netapp,dc=com" `
+    -SearchBindDN "CN=Administrator,CN=Users,DC=demo,DC=netapp,DC=com" `
+    -SearchBindPassword 'Netapp1!' `
+    -UserSearchFilter "(&(objectClass=user)(sAMAccountName=%USERNAME%))"
